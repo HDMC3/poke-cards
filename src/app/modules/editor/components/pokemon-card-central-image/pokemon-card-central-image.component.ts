a737db4d5611741be5ec4cdff1848bef1b0b29e3
Hugo Moran <moranh56@gmail.com>
2022-03-12 19:42:35 -0600

    add translation of card texts

--- src/app/modules/editor/components/pokemon-card-central-image/pokemon-card-central-image.component.ts
+++ src/app/modules/editor/components/pokemon-card-central-image/pokemon-card-central-image.component.ts
@@ -4,6 +4,7 @@ import { POKEMON_TYPE_COLORS } from 'src/app/core/constants/pokemon-type-colors'
 import { Pokemon } from 'src/app/core/interfaces/pokemon.interface';
 import { CustomStylesCentralImageCardService } from 'src/app/core/services/custom-styles-central-image-card.service';
 import { CustomValuesCentralImageCard } from 'src/app/core/types/custom-values-central-image-card';
+import { PokemonTypeName } from 'src/app/core/types/pokemon-type-name';
 
 @Component({
     selector: 'app-pokemon-card-central-image',
@@ -33,6 +34,44 @@ export class PokemonCardCentralImageComponent implements OnInit, OnDestroy {
 
     getPercentStatProgressBar(baseStat: number) {
         return Number((baseStat / 255).toFixed(2)) * 100;
+    }
+
+    getTranslateStatName(statName: string) {
+        const dictStatNames: {[key: string] : string} = {
+            hp: 'hp',
+            attack: 'ataque',
+            defense: 'defensa',
+            speed: 'velocidad'
+        };
+
+        return dictStatNames[statName];
+    }
+
+    getTranslatePokemonTypeName(pokemonTypeName: PokemonTypeName) {
+        const dictPokemonTypeNames = {
+            normal: 'normal',
+            fighting: 'lucha',
+            flying: 'volador',
+            poison: 'veneno',
+            ground: 'tierra',
+            rock: 'roca',
+            bug: 'bicho',
+            ghost: 'fantasma',
+            steel: 'acero',
+            fire: 'fuego',
+            water: 'agua',
+            grass: 'planta',
+            electric: 'electrico',
+            psychic: 'psiquico',
+            ice: 'hielo',
+            dragon: 'dragon',
+            dark: 'siniestro',
+            fairy: 'hada',
+            unknown: '???',
+            shadow: 'shadow'
+        };
+
+        return dictPokemonTypeNames[pokemonTypeName];
     }
 
 }
